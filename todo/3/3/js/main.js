41b99fe7627a8d50d08bafa91dd264e4d8f12431
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2022-07-21 12:03:08 +0900

    3-1～3-4まで済

---
+++ todo/3/3/js/main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+{
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+     fetch('?action=toggle', {
+        method: 'POST',
+        body: new URLSearchParams({
+          id: checkbox.dataset.id,
+          token: checkbox.dataset.token,
+        }),
+      });
+
+      // checkbox.nextElementSibling.classList.toggle('done');
+    });
+  });
+
+  const deletes = document.querySelectorAll('.delete');
+  deletes.forEach(span => {
+    span.addEventListener('click', () => {
+      if (!confirm('Are you sure?')) {
+        return;
+      }
+      
+      fetch('?action=delete', {
+        method: 'POST',
+        body: new URLSearchParams({
+          id: span.dataset.id,
+          token: span.dataset.token,
+        }),
+      });
+
+      span.parentNode.remove();
+    });
+  });
+
+  const purge = document.querySelector('.purge');
+  purge.addEventListener('click', () => {
+    if (!confirm('Are you sure?')) {
+      return;
+    }
+
+    fetch('?action=purge', {
+      method: 'POST',
+      body: new URLSearchParams({
+        token: purge.dataset.token,
+      }),
+    });
+
+    const lis = document.querySelectorAll('li');
+    lis.forEach(li => {
+      if (li.children[0].checked) {
+        li.remove();
+      }
+    });
+  });
+}
