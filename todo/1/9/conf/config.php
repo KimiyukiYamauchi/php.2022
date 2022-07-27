f48c335cfaefac2e40ddd0885585e0caed46d64d
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2022-07-28 08:43:03 +0900

    rename

--- todo/1/9/conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-
-define('DSN', 'mysql:host=localhost;dbname=myapp;charset=utf8mb4');
-define('DB_USER', 'myappuser');
-define('DB_PASS', 'myapppass');
-define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/todo/');
