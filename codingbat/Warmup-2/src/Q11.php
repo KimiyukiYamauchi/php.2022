f44e9fba38b5a62aba1876b344cef952d70d1b14
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2023-03-16 08:18:23 +0900

    Warmup-2

---
+++ codingbat/Warmup-2/src/Q11.php
@@ -0,0 +1,24 @@
+<?php
+
+class Q11
+{
+    /**
+     * 与えられた2つの文字列(string型)で共通する2文字の文字列の個数を返す関数
+     * 共通する文字列は文字位置も一致しなければいけない
+     * 例えば"xxcaazz"と"xxbaaz"が与えられた場合、"xx"(文字位置0)、"aa"(文字位置3)、
+     * "az"(文字位置4)が共通なので、3を返す
+     * 例：
+     * 　stringMatch("xxcaazz", "xxbaaz") → 3
+     * 　stringMatch("abc", "abc") → 2
+     * 　stringMatch("abc", "axc") → 0
+     * 
+     * @param array string $a string $b
+     * 
+     * @return 上記の条件が成り立つ個数を返す
+     * 
+     */
+    public function stringMatch(string $a, string $b) : int
+    {
+        return -1;
+    }
+}
---
+++ codingbat/Warmup-2/src/Q16.php
@@ -0,0 +1,22 @@
+<?php
+
+class Q16
+{
+    /**
+     * 与えられたint型の配列の要素に連続して3つ同じ数値がある場合はfalseを返す
+     * それ以外はtrueを返す関数
+     * 例：
+     * 　noTriples([1, 1, 2, 2, 1]) → true
+     * 　noTriples([1, 1, 2, 2, 2, 1]) → false
+     * 　noTriples([1, 1, 1, 2, 2, 2, 1]) → false
+     * 
+     * @param array array $nums
+     * 
+     * @return 上記の条件に従ってbool値(trueまたはfalse)を返す
+     * 
+     */
+    public function noTriples(array $nums) : bool
+    {
+        return false;
+    }
+}
